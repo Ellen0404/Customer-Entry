52b28df48c3491130ac468e180380a24793f5c0a
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-08-11 13:28:21 -0600

    Updates

---
+++ precache-manifest.f8d003455260e5b822220bbf97668670.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbb6b6300e357ec1e31ffdf38d96df1",
+    "url": "/Customer-Entry/index.html"
+  },
+  {
+    "revision": "e5c2d438797fd53e9794",
+    "url": "/Customer-Entry/static/css/main.4763b4d3.chunk.css"
+  },
+  {
+    "revision": "ccb6ff8747525e6da034",
+    "url": "/Customer-Entry/static/js/2.f8fbc71f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Customer-Entry/static/js/2.f8fbc71f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5c2d438797fd53e9794",
+    "url": "/Customer-Entry/static/js/main.b3c0c5d6.chunk.js"
+  },
+  {
+    "revision": "2bcb820f71b6ec861809",
+    "url": "/Customer-Entry/static/js/runtime-main.e09d7f06.js"
+  }
+]);
