fdd2dbe47572a94e0ef8cf74add1a7c05765bc03
Elena Demidionok <elenademidionok@Elenas-MacBook-Pro.local>
2020-08-11 16:09:09 -0600

    Updates

--- precache-manifest.ba4b63f0390c036439beeac1245a6327.js
+++ precache-manifest.ba4b63f0390c036439beeac1245a6327.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cdbb6b6300e357ec1e31ffdf38d96df1",
+    "revision": "0745faa868159e52ab9d6c740f8a17da",
     "url": "/Customer-Entry/index.html"
   },
   {
-    "revision": "e5c2d438797fd53e9794",
+    "revision": "9e0a518aae05320b074b",
     "url": "/Customer-Entry/static/css/main.4763b4d3.chunk.css"
   },
   {
-    "revision": "ccb6ff8747525e6da034",
-    "url": "/Customer-Entry/static/js/2.f8fbc71f.chunk.js"
+    "revision": "b16a000ce5ba41765b89",
+    "url": "/Customer-Entry/static/js/2.15762917.chunk.js"
   },
   {
     "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
-    "url": "/Customer-Entry/static/js/2.f8fbc71f.chunk.js.LICENSE.txt"
+    "url": "/Customer-Entry/static/js/2.15762917.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e5c2d438797fd53e9794",
-    "url": "/Customer-Entry/static/js/main.b3c0c5d6.chunk.js"
+    "revision": "9e0a518aae05320b074b",
+    "url": "/Customer-Entry/static/js/main.98b2b2c7.chunk.js"
   },
   {
     "revision": "2bcb820f71b6ec861809",
